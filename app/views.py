30ba1552fcd4d37ffd94896644087d0f30ecee08
mshilpa54321 <mshilpa54321@gmail.com>
2023-06-10 14:20:09 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def mdb4(request):
+    return render(request,'mdb4.html')
+
+def bg_image(request):
+    return render(request,'bg_image.html')
